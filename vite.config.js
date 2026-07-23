a1611d0c9610f5c29a64b75b561d33000fb97496
bartoszcisek11-gg <bartoszcisek11@gmail.com>
2026-07-23 18:38:36 +0200

    Aktualizacja PWA Offline

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/CentrumDowodzenia/', // <--- Nazwa Twojego repozytorium na GitHubie
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Centrum Dowodzenia',
+        short_name: 'Centrum',
+        description: 'Moja osobista aplikacja',
+        theme_color: '#1a1d24',
+        background_color: '#1a1d24',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
+  base: '/CentrumDowodzenia/',
+});
